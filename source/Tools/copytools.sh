36583a0a2d18cbcdf273a65494c0f54af2f9c603
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-10-10 15:08:05 -0400

    fixup linux copytools script

--- source/Tools/copytools.sh
+++ source/Tools/copytools.sh
@@ -2,26 +2,19 @@
 
 FSharpVersion=4.0.1.10
 ScriptCSVersion=0.16.1
-AzurePowershellVersion=1.6.0
 
 ToolsFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 FSharpFolder=${ToolsFolder}/FSharp.Compiler.Tools.${FSharpVersion}/
 if [ ! -d "$FSharpFolder" ]; then
     echo Copying FSharp to Tools folder
-    copy -r ${HOME}/.nuget/packages/FSharp.Compiler.Tools/${FSharpVersion}/tools ${FSharpFolder}
+    cp -r ${HOME}/.nuget/packages/FSharp.Compiler.Tools/${FSharpVersion}/tools ${FSharpFolder}
 fi
 
 ScriptCSFolder=${ToolsFolder}/ScriptCS.${ScriptCSVersion}/
 if [ ! -d "$ScriptCSFolder" ]; then
     echo Copying ScriptCS to Tools folder
-    copy -r ${HOME}/.nuget/packages/ScriptCS/${ScriptCSVersion}/tools ${ScriptCSFolder}
-fi
-
-AzurePowershellFolder=${ToolsFolder}/Octopus.Dependencies.AzureCmdlets.${AzurePowershellVersion}/
-if [[ $1 == azure && ! -d "$AzurePowershellFolder" ]]
-    echo Copying Azure PowerShell to Tools folder
-    copy -r ${HOME}/.nuget/packages/Octopus.Dependencies.AzureCmdlets/${AzurePowershellVersion}/tools ${AzurePowershellFolder}
+    cp -r ${HOME}/.nuget/packages/ScriptCS/${ScriptCSVersion}/tools ${ScriptCSFolder}
 fi
 
 exit 0
