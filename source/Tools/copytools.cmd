7d90c2454bfd951d31a758b4dc85929a01a283ef
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-10-11 14:31:23 +1000

    copy tools from nuget cache in $home

---
+++ source/Tools/copytools.cmd
@@ -0,0 +1,28 @@
+@echo off
+echo %1
+SET FSharpVersion=4.0.1.10
+SET ScriptCSVersion=0.16.1
+SET AzurePowershellVersion=1.6.0
+SET ToolsFolder=%~dp0
+
+SET FSharpFolder=%ToolsFolder%FSharp.Compiler.Tools.%FSharpVersion%\
+IF EXIST %FSharpFolder% GOTO FSHARPEXISTS
+   echo Copying FSharp to Tools folder
+   xcopy /E %userprofile%\.nuget\packages\FSharp.Compiler.Tools\%FSharpVersion%\tools %FSharpFolder%
+:FSHARPEXISTS
+
+SET ScriptCSFolder=%ToolsFolder%ScriptCS.%ScriptCSVersion%\
+IF EXIST %ScriptCSFolder% GOTO SCRIPTCSEXISTS
+   echo Copying ScriptCS to Tools folder
+   xcopy /E %userprofile%\.nuget\packages\ScriptCS\%ScriptCSVersion%\tools %ScriptCSFolder%
+:SCRIPTCSEXISTS
+
+IF "%1" NEQ "azure" GOTO DONE
+SET AzurePowershellFolder=%ToolsFolder%Octopus.Dependencies.AzureCmdlets.%AzurePowershellVersion%\
+IF EXIST %AzurePowershellFolder% GOTO AZUREPOWERSHELLEXISTS
+   echo Copying Azure PowerShell to Tools folder
+   xcopy /E %userprofile%\.nuget\packages\Octopus.Dependencies.AzureCmdlets\%AzurePowershellVersion%\PowerShell %AzurePowershellFolder%
+:AZUREPOWERSHELLEXISTS
+
+:DONE
+exit 0
